40c71969ba571185e6ab064bee9bb9f920a1390e
ARNAB KUMAR GHOSH <arnab.kumarghosh.20@aot.edu.in>
2023-03-04 00:28:38 +0530

    Added my project

--- Javascript/chompa.js
+++ Javascript/chompa.js
@@ -1,3 +1,12 @@
+let btnback= document.querySelector('button');
+btnback.addEventListener('click', () =>{
+  window.history.back();
+})
+
+
+
+
+
 function setThemePreference() {
     var d = new Date();
     /*
